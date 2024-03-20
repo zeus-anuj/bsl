9270f95a85a021f6d74cbd73a66e86065815b89f
ANUJ PANDIT <89330438+zeus-anuj@users.noreply.github.com>
2024-03-20 10:40:52 +0000

    new

---
+++ bsl-website-backend/frontend/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            {/* Add other components or content here */}
+        </div>
+    );
+}
+
+export default App;
---
+++ bsl-website-backend/frontend/src/Components/header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+import Navbar from './navbar';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>My App</h1>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Header;
